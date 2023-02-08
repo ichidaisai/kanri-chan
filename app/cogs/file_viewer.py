3eb3e4442c69b648c18c47937056127219254762
Huyu2239 <huyu2239@gmail.com>
2023-02-08 23:52:27 +0900

    add: pdf -> jpeg

--- app/cogs/file_viewer.py
+++ app/cogs/file_viewer.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 import discord
+import pdf2image
+import asyncio
 import io
 
 
@@ -14,7 +16,17 @@ class FileViewer(commands.Cog):
         if message.channel.type != discord.ChannelType.text:
             return
         thread = await message.create_thread(name=message.attachments[0].filename)
+        attachment = message.attachments[0]
+        loop = asyncio.get_running_loop()
         images = []
+        # pdf -> jpeg
+        if attachment.content_type == "application/pdf":
+            pdf_io = io.BytesIO()
+            await attachment.save(pdf_io)
+            images = await loop.run_in_executor(
+                None, pdf2image.convert_from_bytes, pdf_io.read()
+            )
+
         images = [images[idx : idx + 10] for idx in range(0, len(images), 10)]
         count = 1
         for image_container in images:
