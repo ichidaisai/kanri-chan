5330d6e01e37b3685f39f158659dadad7f90b28a
Huyu2239 <huyu2239@gmail.com>
2023-02-24 02:59:30 +0900

    add: RoleManagerの処理部分&DB(group_table)実装

--- app/cogs/role_manager.py
+++ app/cogs/role_manager.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+from .mylib import database
 
 
 class RoleManager(commands.Cog):
@@ -18,6 +19,17 @@ class RoleManager(commands.Cog):
             return await ctx.send("新規作成する団体の出店形式が指定されていません。\n例) `!role make 屋外 団体名A`")
         if group_name is None:
             return await ctx.send("新規作成する団体名が指定されていません。\n例) `!role make 屋外 団体名A`")
+        if database.is_group_exist(group_name, group_type):
+            return await ctx.send("既に存在する団体名です。")
+
+        category = discord.utils.get(self.bot.guild.categories, name=group_type)
+        channel = await category.create_text_channel(name=group_name)
+        role = await self.bot.guild.create_role(name=group_name)
+        await channel.set_permissions(role, view_channel=True)
+
+        database.group_table.insert(dict(role_id=role.id, name=group_name, type=group_type, channel_id=channel.id))
+        group = database.Group(role_id=role.id)
+        await ctx.send(vars(group))
         await ctx.send(f"{group_type}に{group_name}を作成")
     
     @role.command()
@@ -26,6 +38,15 @@ class RoleManager(commands.Cog):
             return await ctx.send("削除する団体の出店形式が指定されていません。\n例) `!role delete 屋外 団体名A`")
         if group_name is None:
             return await ctx.send("削除する団体名が指定されていません。\n例) `!role delete 屋外 団体名A`")
+        if not database.is_group_exist(group_name, group_type):
+            return await ctx.send("存在しない団体名です。")
+        
+        group = database.Group(name=group_name, type=group_type)
+        group.delete()
+        channel = self.bot.guild.get_channel(group.channel_id)
+        await channel.delete()
+        role = self.bot.guild.get_role(group.role_id)
+        await role.delete()
         await ctx.send(f"{group_type}の{group_name}を削除")
 
     @role.command()
@@ -36,6 +57,18 @@ class RoleManager(commands.Cog):
             return await ctx.send("名称を変更する団体名が指定されていません。\n例) `!role rename 屋外 団体名A 団体名B`")
         if new_name is None:
             return await ctx.send("新しい名称が指定されていません。\n例) `!role rename 屋外 団体名A 団体名B`")
+        if not database.is_group_exist(group_name, group_type):
+            return await ctx.send("存在しない団体が名称を変更する団体として指定されました。")
+        if database.is_group_exist(new_name, group_type):
+            return await ctx.send("既に存在している団体名が新しい名称として指定されました。")
+        
+        group = database.Group(name=group_name, type=group_type)
+        group.name = new_name
+        group.update()
+        channel = self.bot.guild.get_channel(group.channel_id)
+        await channel.edit(name=new_name)
+        role = self.bot.guild.get_role(group.role_id)
+        await role.edit(name=new_name)
         await ctx.send(f"{group_type}の{group_name}を{new_name}に変更")
 
 async def setup(bot):
