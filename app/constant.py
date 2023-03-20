0bcd623d90bc1fc126881db10c96e20ef5a7d833
Huyu2239 <huyu2239@gmail.com>
2023-03-20 11:28:21 +0900

    fix: 不要

--- app/constant.py
+++ app/constant.py
@@ -10,12 +10,3 @@ try:
     GOOGLE_DRIVE_FOLDER_ID = getenv("GOOGLE_DRIVE_FOLDER_ID")
 except Exception:
     raise Exception("必要な環境変数が正しく指定されていません。")
-
-if not isinstance(TOKEN, str):
-    raise Exception("TOKEN が正しく指定されていません。")
-if not isinstance(SERVER_ID, int):
-    raise Exception("SERVER_ID が正しく指定されていません。")
-if not isinstance(RELAYING_CATEGORY_ID, int):
-    raise Exception("RELAYING_CATEGORY_ID が正しく指定されていません。")
-if not isinstance(GOOGLE_DRIVE_FOLDER_ID, str):
-    raise Exception("RELAYING_CATEGORY_ID が正しく指定されていません。")
